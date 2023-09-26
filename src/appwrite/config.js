60501feb4390155767f4c1f27cc396d539dac968
Ajay Prasad <ajay.prasad@outlook.in>
2023-09-26 10:36:53 +0530

    updated config.js

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -94,11 +94,35 @@ export class Service{
 
     async uploadFile(file){
         try {
-            
+            return await this.bucket.createFile(
+                conf.appwriteBucketId,
+                ID.unique(),
+                file
+            )
         } catch (error) {
             console.log("Appwrite service :: uploadFile :: error", error);
             return false;
         }
+    }
+
+    async deleteFile(fileId){
+        try {
+            await this.bucket.deleteFile(
+                conf.appwriteBucketId,
+                fileId
+            )
+            return true;
+        } catch (error) {
+            console.log("Appwrite service :: deleteFile :: error", error);
+            return false;
+        }
+    }
+
+    getFilePreview(fileId){
+        return this.bucket.getFilePreview(
+            conf.appwriteBucketId,
+            fileId
+        )
     }
 }
 
