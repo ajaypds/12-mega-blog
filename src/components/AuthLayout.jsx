1bcfeab48b037ad7bf9bfbccd4c74a5f0fd7daaf
Ajay Prasad <ajay.prasad@outlook.in>
2023-10-01 16:37:35 +0530

    added AuthLayout.jsx

---
+++ src/components/AuthLayout.jsx
@@ -0,0 +1,28 @@
+import React, {useEffect, useState} from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
+const AuthLayout = ({children, authentication = true}) => {
+    const navigate = useNavigate()
+    const [loader, setLoader] = useState(true)
+    const authStatus = useSelector((state) => state.status)
+
+    useEffect(() => {
+        //TODO: make it more easy to understand
+
+        
+
+        if(authentication && authStatus !== authentication){
+            navigate("/login")
+        }else if(!authentication && authStatus !== authentication){
+            navigate("/")
+        }
+        setLoader(false)
+    }, [authStatus, navigate, authentication])
+
+  return (
+    <div>AuthLayout</div>
+  )
+}
+
+export default AuthLayout
