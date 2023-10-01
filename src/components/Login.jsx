b30c770ebe4d752f4657a84e34c8ccf915b3be3c
Ajay Prasad <ajay.prasad@outlook.in>
2023-10-01 16:12:52 +0530

    created Signup.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -45,7 +45,7 @@ const Login = () => {
                 <form onSubmit={handleSubmit(login)} className='mt-8' >
                     <div className='space-y-5' >
                         <Input 
-                        label="Email"
+                        label="Email: "
                         placeholder="Enter your email"
                         type="email"
                         {...register("email", {
